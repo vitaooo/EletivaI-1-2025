7e9608efe535fa2536050444fa15c0e055e6ced5
Victor <victorhvercosag@gmail.com>
2025-09-04 22:04:18 -0300

    terminando o exercicio 3 04-09-25

--- Lista3/cabecalho.php
+++ Lista3/cabecalho.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lista de exercício 2</title>
+    <title>Lista de exercício 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="#">Lista de exercício 2</a>
+            <a class="navbar-brand" href="#">Lista de exercício 3</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -28,11 +28,6 @@
                             <li><a class="dropdown-item" href="ex3.php">ex3</a></li>
                             <li><a class="dropdown-item" href="ex4.php">ex4</a></li>
                             <li><a class="dropdown-item" href="ex5.php">ex5</a></li>
-                            <li><a class="dropdown-item" href="ex6.php">ex6</a></li>
-                            <li><a class="dropdown-item" href="ex7.php">ex7</a></li>
-                            <li><a class="dropdown-item" href="ex8.php">ex8</a></li>
-                            <li><a class="dropdown-item" href="ex9.php">ex9</a></li>
-                            <li><a class="dropdown-item" href="ex10.php">ex10</a></li>
                         </ul>
                     </li>
                 </ul>
---
+++ Lista3/ex1.php
@@ -0,0 +1,28 @@
+<?php
+include("cabecalho.php");
+
+echo "<h1>Exercício 1</h1>";
+?>
+<h1></h1>
+<form method="post">
+    <div class="mb-3">
+        <label for="word" class="form-label">Digite uma palavra</label>
+        <input type="text" id="word" name="word" class="form-control" required="">
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $word = $_POST['word'];
+    function caracteres($palavra)
+    {
+        $caract = strlen($palavra);
+        echo "A palavra ($palavra) tem $caract caracteres";
+    }
+    caracteres($word);
+}
+
+
+
+include("rodape.php");
+?>
