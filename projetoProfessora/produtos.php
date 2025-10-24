45877dd69089a86b617e0e032e0172f9dc5d4684
Victor <victorhvercosag@gmail.com>
2025-10-23 22:05:31 -0300

    fazendo o crud dos produtos

---
+++ projetoProfessora/produtos.php
@@ -0,0 +1,64 @@
+<?php
+    require("cabecalho.php");
+    require("conexao.php");
+    try{
+        $stmt = $pdo->query("SELECT c.nome, p.* FROM produto p 
+            INNER JOIN categoria c ON c.id = p.categoria_id");
+        $dados = $stmt->fetchAll();
+
+    } catch(\Exception $e) {
+        echo "Erro: ".$e->getMessage();
+    }
+
+    if (isset($_GET['cadastro']) && $_GET['cadastro']){
+        echo "<p clas=='text-success'>Cadastro realizado!</p>";
+    } else if (isset($_GET['cadastro']) && !$_GET['cadastro']) {
+        echo "<p class='text-danger'>Erro ao cadastrar!</p>";
+    }
+
+    if (isset($_GET['editar']) && $_GET['editar']){
+      echo "<p clas=='text-success'>Registro editado!</p>";
+    } else if (isset($_GET['editar']) && !$_GET['editar']) {
+      echo "<p class='text-danger'>Erro ao editar!</p>";
+    }
+    if (isset($_GET['excluir']) && $_GET['excluir']){
+      echo "<p clas=='text-success'>Categoria excluída!</p>";
+    } else if (isset($_GET['excluir']) && !$_GET['excluir']) {
+      echo "<p class='text-danger'>Erro ao excluir!</p>";
+    }
+?>
+
+    
+<h2>Produtos</h2>
+          <a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro</a>
+          <table class="table table-hover table-striped">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach($dados as $d): 
+                ?>
+                <tr>
+                  <td><?= $d['id'] ?></td>
+                  <td><?= $d['descricao'] ?></td>
+                  <td><?= $d['nome'] ?></td>
+                  <td class="d-flex gap-2">
+                    <a href="editar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
+                    <a href="consultar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
+                  </td>
+                </tr>
+                <?php 
+                    endforeach; 
+                ?>
+            </tbody>
+          </table>
+        
+
+<?php
+    require("rodape.php");
+?>
