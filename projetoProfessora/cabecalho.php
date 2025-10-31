9b100535c16a7d77ea4b383d38e30c02f2ae5a99
Victor <victorhvercosag@gmail.com>
2025-10-30 21:22:43 -0300

    Terminando o consultar produtos

--- projetoProfessora/cabecalho.php
+++ projetoProfessora/cabecalho.php
@@ -11,10 +11,18 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        @media print {
+            .no-print{
+                display: none !important;    
+            }
+        }
+    </style>
+
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark no-print">
         <div class="container">
             <a class="navbar-brand" href="principal.php">Sistema</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
--- projetoProfessora/produtos.php
+++ projetoProfessora/produtos.php
@@ -30,13 +30,22 @@
 
     
 <h2>Produtos</h2>
-          <a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro</a>
+          <a href="novo_produto.php" class="btn btn-success mb-3 no-print">Novo Registro</a>
           <table class="table table-hover table-striped">
             <thead>
               <tr>
+                <th colspan="3">Dados dos produtos</th>
+                <th class="no-print">
+                  <button class="btn btn-secondary" onclick="window.print()">
+                    Imprimir
+                  </button>
+                </th>
+              </tr>
+              <tr>
                 <th>ID</th>
                 <th>Nome</th>
-                <th>Ações</th>
+                <th>Categorias</th>
+                <th class="no-print">Ações</th>
               </tr>
             </thead>
             <tbody>
@@ -47,7 +56,7 @@
                   <td><?= $d['id'] ?></td>
                   <td><?= $d['descricao'] ?></td>
                   <td><?= $d['nome'] ?></td>
-                  <td class="d-flex gap-2">
+                  <td class="d-flex gap-2 no-print">
                     <a href="editar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
                     <a href="consultar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
                   </td>
